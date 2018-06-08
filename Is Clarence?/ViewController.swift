6009cc50b3369dc3ef6cd3ba381dd70ac0832d98
Clarence Ji <clare.dev@clarenceji.net>
2018-06-07 17:45:31 -0700

    Initial Commit

---
+++ Is Clarence?/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Is Clarence?
+//
+//  Created by Clarence Ji on 6/7/18.
+//  Copyright © 2018 Clarence Ji. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
